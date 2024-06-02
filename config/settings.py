04bbdac13dc61238620b4652c6d5257162c4ff31
aftab <aftabahmad41442@gmail.com>
2024-06-02 20:40:27 +0530

    allowed host

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9nfo6!y-i@elp-2fc@rw()m-fhc$0jy%&f+9=)ew)*=!r$q#e#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
