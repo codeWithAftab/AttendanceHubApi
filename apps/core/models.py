650d8547970bee20d8bc104291ddb8d8c2b6617d
aftab <aftabahmad41442@gmail.com>
2024-07-21 20:12:44 +0530

    custom error craetion function

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class CustomErrors(models.Model):
-    code = models.CharField(max_length=122)
+    code = models.CharField(max_length=122, unique=True)
     status_code = models.IntegerField()
     detail = models.TextField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     count = models.PositiveIntegerField(default=1)
+
 
     def __str__(self) -> str:
         return f"{self.code}"
--- scripts/errors.py
+++ scripts/errors.py
@@ -1,5 +1,34 @@
-[
-    "MissingFieldError",
-    "EmailAlreadyExist",
-    "WrongEmployeeId"
+from apps.core.models import CustomErrors
+
+error_messages = [
+    {"code": "InterchangeRequestNotFound", "message": "Interchange request not found", "status": 400},
+    {"code": "SameDayMustInInterchange", "message": "Same day is must", "status": 400},
+    {"code": "ShiftInterchangeRequestAlreadyPending", "message": "Same Shift Interchange Request Already Pending", "status": 400},
+    {"code": "TargetedShiftNotFound", "message": "Targeted Shift not found", "status": 400},
+    {"code": "RequesterShiftNotFound", "message": "Requester shift not found error", "status": 400},
+    {"code": "StaffUserNotFound", "message": "Staff User not found with provided email", "status": 400},
+    {"code": "AttendanceAlreadyMarked", "message": "User already marked their attendance", "status": 400},
+    {"code": "OutOfAttendanceWindow", "message": "Attendance can only be marked within 1 hour of your shift end time", "status": 400},
+    {"code": "CannotAssignWeekOffShift", "message": "Manager can only assign shift on weekdays not for weekends", "status": 400},
+    {"code": "WeeklyOffToday", "message": "Weekly off today", "status": 400},
+    {"code": "NoShiftForToday", "message": "No shift for today", "status": 400},
+    {"code": "OutOfShiftHours", "message": "Out of shift hours", "status": 400},
+    {"code": "WrongEmployeeId", "message": "Employee ID is not correct, please check", "status": 400},
+    {"code": "PermissionError", "message": "User does not have required permissions to perform this task", "status": 401},
+    {"code": "MissingFieldError", "message": "These required fields must be present", "status": 400},
+    {"code": "EmailAlreadyExist", "message": "This email already exists in the database", "status": 403}
 ]
+
+
+def create_errors():
+    custom_error_objs = []
+    
+    for error_message in error_messages:
+        err = CustomErrors(code=error_message["code"], status_code=error_message["status"], detail=error_message["message"])
+        custom_error_objs.append(err)
+
+    try:
+        CustomErrors.objects.bulk_create(custom_error_objs)
+        print("creation done.")
+    except Exception as e:
+        print(f"Custom Errors already created.. {e}")
